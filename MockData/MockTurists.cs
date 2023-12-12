8b8f889c20bb3b2484969fb7f91ccbe7d13c00d4
Salah Eddin Salam <144003465+SalahSalam@users.noreply.github.com>
2023-12-12 09:21:03 +0100

    Add files via upload

---
+++ MockData/MockTurists.cs
@@ -0,0 +1,29 @@
+﻿using TuristInfoV1.Models;
+using TuristInfoV1.MockData;
+using Microsoft.Extensions.Hosting;
+
+namespace TuristInfoV1.MockData
+{
+	public class MockTurist
+	{
+		#region Static Fields
+		private static List<Turist> _turists = new List<Turist>();
+			//{
+			///new Turist (1,
+			/// <summary>
+			/// new Turist (2
+			/// </summary>new Turist,
+				///Per
+		///	new turist(3,
+			///	Christian
+		    ///};
+			#endregion
+
+			#region Methods
+			public static List<Turist> GetMockTurist()
+			{
+				return _turists;
+			}
+			#endregion
+		}
+	}
