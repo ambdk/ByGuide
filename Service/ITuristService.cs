febe1c8a49ad74f0e97a61cf66d9b2ff8ff052be
Salah Eddin Salam <144003465+SalahSalam@users.noreply.github.com>
2023-12-12 09:26:47 +0100

    Add files via upload

---
+++ Service/ITuristService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Hosting;
+using TuristInfoV1.Models;
+
+
+namespace TuristInfoV1.Services
+{
+	public interface ITuristService
+	{
+		void AddTurist(Turist turist);
+		IEnumerable<Turist> TuristSearch(string name);
+		void UpdateTurist(Turist turist);
+		Turist DeleteTurist(int? id);
+		Turist GetTurist(int Id);
+		List<Turist> GetTurists();
+
+	}	
+}
